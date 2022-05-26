a039ac7f17f32f5b283cfcbec80b3163ec22a95a
Aditya Mehra <aix.m@outlook.com>
2022-05-26 16:08:57 +0930

    Improve event documentation

--- ovos_PHAL_plugin_network_manager/__init__.py
+++ ovos_PHAL_plugin_network_manager/__init__.py
@@ -13,26 +13,62 @@ from ovos_utils.log import LOG
 # ===================
 # Backend:
 # ovos.phal.nm.set.backend
+# - type: Request
+# - description: Allows client to use a specific backend
+#
 # ovos.phal.nm.backend.not.supported
+# - type: Response
+# - description: Emitted when plugin does not support the
+# specific backend
 #
 # Scanning: 
 # ovos.phal.nm.scan
+# - type: Request
+# - description: Allows client to request for a network scan
+#
 # ovos.phal.nm.scan.complete
+# - type: Response
+# - description: Emited when the requested scan is completed
+# with a network list
 #
 # Connecting:
-# Request: ovos.phal.nm.connect
+# ovos.phal.nm.connect
+# - type: Request
+# - description: Allows clients to connect to a given network
+#
 # ovos.phal.nm.connection.successful
+# - type: Response
+# - description: Emitted when a connection is successfully established
+#
 # ovos.phal.nm.connection.failure
+# - type: Response
+# - description: Emitted when a connection fails to establish
 #
 # Disconnecting:
 # ovos.phal.nm.disconnect
+# - type: Request
+# - description: Allows clients to disconnect from a network
+#
 # ovos.phal.nm.disconnection.successful
+# - type: Response
+# - description: Emitted when a connection successfully disconnects
+#
 # ovos.phal.nm.disconnection.failure
+# - type: Response
+# - description: Emitted when a connection fails to disconnect
 #
 # Forgetting:
 # ovos.phal.nm.forget
+# - type: Request
+# - description: Allows a client to forget a network
+#
 # ovos.phal.nm.forget.successful
+# - type: Response
+# - description: Emitted when a connection successfully is forgetten
+#
 # ovos.phal.nm.forget.failure
+# - type: Response
+# - description: Emitted when a connection fails to forget
 
 class NetworkManagerPlugin(PHALPlugin):
 
